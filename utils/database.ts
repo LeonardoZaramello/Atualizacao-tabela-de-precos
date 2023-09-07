496bf8a8eb80693041905ec705b60ddcdfb02455
Leonardo Zaramello <leeurw@hotmail.com>
2023-09-07 19:16:10 -0300

    Feat: Database connection

---
+++ utils/database.ts
@@ -0,0 +1,21 @@
+import mysql from "mysql2/promise";
+
+export default async function query({query, values = []}:any){
+  const dbConnection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
+  })
+
+  try {
+    const [results] = await dbConnection.execute(query, values)
+    await dbConnection.end()
+    return results;
+
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
+}
