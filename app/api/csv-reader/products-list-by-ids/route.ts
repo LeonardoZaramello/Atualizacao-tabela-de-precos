3523762cee86158a0fc7bdc196ba2bb144635653
Leonardo Zaramello <leeurw@hotmail.com>
2023-09-08 03:27:31 -0300

    Feat: Back-end get products to update endpoint

---
+++ app/api/csv-reader/products-list-by-ids/route.ts
@@ -0,0 +1,36 @@
+import excuteQuery from '@/utils/database';
+import { FieldPacket, OkPacket, OkPacketParams, RowDataPacket } from 'mysql2/promise';
+
+type result = {
+    code: number,
+    name: string,
+    cost_price: string,
+    sales_price: string
+    new_price: string
+}
+export async function POST(req: Request, res:Response) {
+    try {    
+        const jsonData = await req.json();
+        const products = jsonData.products;
+     
+        const sqlQueryValues = []
+        for (let index = 0; index < products.length; index++) {
+            sqlQueryValues.push(products[index].product_code);
+        }
+        
+        const result = await excuteQuery({
+            query: `SELECT * FROM shopper.products WHERE code IN (${sqlQueryValues.toString()});`,
+        })as [RowDataPacket,FieldPacket[], result[]];
+
+        for (let index = 0; index < result.length; index++) {
+            const element:result|any = result[index];
+            element.new_price= products[index].new_price
+        }
+        console.log(result);
+        
+        return new Response(JSON.stringify(result));
+        
+    } catch ( error ) {
+        console.log( error );
+    }
+};
--- app/api/csv-reader/route.ts
+++ app/api/csv-reader/route.ts
@@ -1,8 +1,9 @@
 import excuteQuery from '@/utils/database';
 
+
 export async function GET(req: Request, res:Response) {
     try {
-        console.log("req GET", req.body)
+        const jsonData = await req.json();
 
         // const result = await excuteQuery({
         //     query: 'SELECT * FROM products;',
@@ -18,15 +19,17 @@ export async function GET(req: Request, res:Response) {
 export async function POST(req: Request, res:Response) {
     try {        
         const jsonData = await req.json();
-        console.log(jsonData);
+      
+
+        const result = await excuteQuery({
+            query: 'SELECT * FROM products;',
+            values: [],
+        });
+
+        // console.log(result);
+        // console.log(jsonData);
         
-
-        // const result = await excuteQuery({
-        //     query: 'SELECT * FROM products;',
-        //     values: [],
-        // });
-
-        return new Response(jsonData);
+        return new Response(JSON.stringify(jsonData));
     } catch ( error ) {
         console.log( error );
     }
